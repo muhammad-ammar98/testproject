4a7dc1dd21a745f35a0a1cff920957ee19ebc1f4
Ammar <ammarnaseerking@gmail.com>
2024-01-03 21:38:00 -0800

    add text

--- app/src/main/java/com/islamicinternationalincims/littalks/MainActivity.java
+++ app/src/main/java/com/islamicinternationalincims/littalks/MainActivity.java
@@ -10,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        gutfinction();
+    }
+
+    private void gutfinction() {
+
+int a=1;
+a=a+1;
     }
 }
